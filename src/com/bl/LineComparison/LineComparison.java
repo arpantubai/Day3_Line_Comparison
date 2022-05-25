eb090553d7fa88586a2bb80614e3e6277231fb0b
arpantubai <arpankng93@gmail.com>
2022-05-25 14:33:14 +0530

    Added Welcome Messege

---
+++ src/com/bl/LineComparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bl.LineComparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
